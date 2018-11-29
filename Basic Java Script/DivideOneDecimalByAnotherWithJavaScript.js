79abbc86dcb905c1218dbb5a6a45e99f4029d519
Ann <anna.muzykina83@gmail.com>
2018-11-29 17:58:44 +0200

    Add files via upload

---
+++ Basic Java Script/DivideOneDecimalByAnotherWithJavaScript.js
@@ -0,0 +1,6 @@
+/**Divide One Decimal by Another with JavaScript
+Now let's divide one decimal by another.
+
+
+Change the 0.0 so that quotient will equal to 2.2. */
+var quotient = 4.4 / 2.0; // Fix this line
