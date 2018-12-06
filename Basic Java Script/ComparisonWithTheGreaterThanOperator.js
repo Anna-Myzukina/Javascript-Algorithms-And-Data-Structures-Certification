d3fe41ad6a5cdda402b00dc1f935bd97f565213a
Ann <anna.muzykina83@gmail.com>
2018-12-06 20:08:37 +0200

    Add files via upload

--- Basic Java Script/ComparisonWithTheGreaterThanOperator.js
+++ Basic Java Script/ComparisonWithTheGreaterThanOperator.js
@@ -11,3 +11,17 @@ Examples
 '1' > 9 // false
 
 Add the greater than operator to the indicated lines so that the return statements make sense. */
+function testGreaterThan(val) {
+    if (val > 100) {  // Change this line
+      return "Over 100";
+    }
+    
+    if (val > 10) {  // Change this line
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+  }
+  
+  // Change this value to test
+  testGreaterThan(10);
